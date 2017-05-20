2a514f9fde6f4d1b7d27251e79603fb64c6750b9
maugosch <mk34694@gmail.com>
2017-05-20 23:41:18 +0200

    Cleaned up some mess
    Now looks better

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $('#imgLoader').on('click', function() {
-   $('canvas').remove(); 
+    $('canvas').remove();
 });
 
-var img; 
+var img;
 
 document.getElementById('imgLoader').onchange = function handleImage(e) {
 
@@ -11,22 +11,21 @@ document.getElementById('imgLoader').onchange = function handleImage(e) {
     canvas.setHeight(600);
     canvas.setWidth(800);
     var reader = new FileReader();
-      reader.onload = function (event){
+    reader.onload = function (event) {
         var imgObj = new Image();
         imgObj.src = event.target.result;
         imgObj.onload = function () {
-          img = new fabric.Image(imgObj);
-            if (img.height > canvas.height*2 || img.width > canvas.width*2) {
+            img = new fabric.Image(imgObj);
+            if (img.height > canvas.height * 2 || img.width > canvas.width * 2) {
                 img.set({
-                    height: img.height*0.25,
-                    width: img.width*0.25,
-                })
-            } else if ((img.height > canvas.height && img.height <= canvas.height*2) || (img.width > canvas.width && img.width <= canvas.width*2)) {
+                    height: img.height * 0.25,
+                    width: img.width * 0.25,
+                }) } else if ((img.height > canvas.height && img.height <= canvas.height * 2) || (img.width > canvas.width && img.width <= canvas.width * 2)) {
                 img.set({
-                    height: img.height*0.5,
-                    width: img.width*0.5,
+                    height: img.height * 0.5,
+                    width: img.width * 0.5
                 })
-            };
+            } ;
             canvas.centerObject(img);
             canvas.add(img);
             canvas.renderAll();
@@ -42,261 +41,261 @@ function update(jscolor) {
 
 $(document).ready(function () {
 
-  var $reset = $('#resetbtn');
-  var $brightness = $('#brightnessbtn');
-  var $noise = $('#noisebtn');
-  var $sepia = $('#sepiabtn');
-  var $contrast = $('#contrastbtn');
-  var $color = $('#colorbtn');
+    var $reset = $('#resetbtn');
+    var $brightness = $('#brightnessbtn');
+    var $noise = $('#noisebtn');
+    var $sepia = $('#sepiabtn');
+    var $contrast = $('#contrastbtn');
+    var $color = $('#colorbtn');
 
-  var $vintage = $('#vintagebtn');
-  var $lomo = $('#lomobtn');
-  var $emboss = $('#embossbtn');
-  var $tiltshift = $('#tiltshiftbtn');
-  var $radialblur = $('#radialblurbtn');
-  var $edgeenhance = $('#edgeenhancebtn');
+    var $vintage = $('#vintagebtn');
+    var $lomo = $('#lomobtn');
+    var $emboss = $('#embossbtn');
+    var $tiltshift = $('#tiltshiftbtn');
+    var $radialblur = $('#radialblurbtn');
+    var $edgeenhance = $('#edgeenhancebtn');
 
-  var $posterize = $('#posterizebtn');
-  var $clarity = $('#claritybtn');
-  var $orangepeel = $('#orangepeelbtn');
-  var $sincity = $('#sincitybtn');
-  var $sunrise = $('#sunrisebtn');
-  var $crossprocess = $('#crossprocessbtn');
+    var $posterize = $('#posterizebtn');
+    var $clarity = $('#claritybtn');
+    var $orangepeel = $('#orangepeelbtn');
+    var $sincity = $('#sincitybtn');
+    var $sunrise = $('#sunrisebtn');
+    var $crossprocess = $('#crossprocessbtn');
 
-  var $hazydays = $('#hazydaysbtn');
-  var $love = $('#lovebtn');
-  var $grungy = $('#grungybtn');
-  var $jarques = $('#jarquesbtn');
-  var $pinhole = $('#pinholebtn');
-  var $oldboot = $('#oldbootbtn');
-  var $glowingsun = $('#glowingsunbtn');
+    var $hazydays = $('#hazydaysbtn');
+    var $love = $('#lovebtn');
+    var $grungy = $('#grungybtn');
+    var $jarques = $('#jarquesbtn');
+    var $pinhole = $('#pinholebtn');
+    var $oldboot = $('#oldbootbtn');
+    var $glowingsun = $('#glowingsunbtn');
 
-  var $hdr = $('#hdrbtn');
-  var $oldpaper = $('#oldpaperbtn');
-  var $pleasant = $('#pleasantbtn');
+    var $hdr = $('#hdrbtn');
+    var $oldpaper = $('#oldpaperbtn');
+    var $pleasant = $('#pleasantbtn');
 
-  var $save = $('#savebtn');
+    var $save = $('#savebtn');
 
   /* As soon as slider value changes call applyFilters */
-  $("input[type=range]").change(applyFilters);
+    $("input[type=range]").change(applyFilters);
 
 
-  function applyFilters() {
-    var hue = parseInt($('#hue').val());
-    var cntrst = parseInt($('#contrast').val());
-    var vibr = parseInt($('#vibrance').val());
-    var sep = parseInt($('#sepia').val());
+    function applyFilters() {
+        var hue = parseInt($('#hue').val());
+        var cntrst = parseInt($('#contrast').val());
+        var vibr = parseInt($('#vibrance').val());
+        var sep = parseInt($('#sepia').val());
 
-    Caman('#canvas', img, function() {
-      this.revert(false);
-      this.hue(hue).contrast(cntrst).vibrance(vibr).sepia(sep).render();
-    });
-  }
+        Caman('#canvas', img, function() {
+          this.revert(false);
+          this.hue(hue).contrast(cntrst).vibrance(vibr).sepia(sep).render();
+        });
+      }
 
-  /* Creating custom filters */
-  Caman.Filter.register("oldpaper", function() {
-    this.pinhole();
-    this.noise(10);
-    this.orangePeel();
-    this.render();
-  });
+      /* Creating custom filters */
+      Caman.Filter.register("oldpaper", function() {
+        this.pinhole();
+        this.noise(10);
+        this.orangePeel();
+        this.render();
+      });
 
-  Caman.Filter.register("pleasant", function() {
-    this.colorize(60, 105, 218, 10);
-    this.contrast(10);
-    this.sunrise();
-    this.hazyDays();
-    this.render();
-  });
+      Caman.Filter.register("pleasant", function() {
+        this.colorize(60, 105, 218, 10);
+        this.contrast(10);
+        this.sunrise();
+        this.hazyDays();
+        this.render();
+      });
 
-  $reset.on('click', function(e) {
-    $('input[type=range]').val(0);
-    Caman('#canvas', img, function() {
-      this.revert(false);
-      this.render();
-    });
-  });
+      $reset.on('click', function(e) {
+        $('input[type=range]').val(0);
+        Caman('#canvas', img, function() {
+          this.revert(false);
+          this.render();
+        });
+      });
 
-  /* In built filters */
-  $brightness.on('click', function(e) {
-    Caman('#canvas', function() {
-      this.brightness(30).render();
-    });
-  });
+      /* In built filters */
+      $brightness.on('click', function(e) {
+        Caman('#canvas', function() {
+          this.brightness(30).render();
+        });
+      });
 
-  $noise.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.noise(10).render();
-    });
-  });
+      $noise.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.noise(10).render();
+        });
+      });
 
-  $contrast.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.contrast(10).render();
-    });
-  });
+      $contrast.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.contrast(10).render();
+        });
+      });
 
-  $sepia.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.sepia(20).render();
-    });
-  });
+      $sepia.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.sepia(20).render();
+        });
+      });
 
-  $color.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.colorize(60, 105, 218, 10).render();
-    });
-  });
+      $color.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.colorize(60, 105, 218, 10).render();
+        });
+      });
 
-  $vintage.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.vintage().render();
-    });
-  });
+      $vintage.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.vintage().render();
+        });
+      });
 
-  $lomo.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.lomo().render();
-    });
-  });
+      $lomo.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.lomo().render();
+        });
+      });
 
-  $emboss.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.emboss().render();
-    });
-  });
+      $emboss.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.emboss().render();
+        });
+      });
 
-  $tiltshift.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.tiltShift({
-        angle: 90,
-        focusWidth: 600
-      }).render();
-    });
-  });
+      $tiltshift.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.tiltShift({
+            angle: 90,
+            focusWidth: 600
+          }).render();
+        });
+      });
 
-  $radialblur.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.radialBlur().render();
-    });
-  });
+      $radialblur.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.radialBlur().render();
+        });
+      });
 
-  $edgeenhance.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.edgeEnhance().render();
-    });
-  });
+      $edgeenhance.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.edgeEnhance().render();
+        });
+      });
 
-  $posterize.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.posterize(8, 8).render();
-    });
-  });
+      $posterize.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.posterize(8, 8).render();
+        });
+      });
 
-  $clarity.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.clarity().render();
-    });
-  });
+      $clarity.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.clarity().render();
+        });
+      });
 
-  $orangepeel.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.orangePeel().render();
-    });
-  });
+      $orangepeel.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.orangePeel().render();
+        });
+      });
 
-  $sincity.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.sinCity().render();
-    });
-  });
+      $sincity.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.sinCity().render();
+        });
+      });
 
-  $sunrise.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.sunrise().render();
-    });
-  });
+      $sunrise.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.sunrise().render();
+        });
+      });
 
-  $crossprocess.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.crossProcess().render();
-    });
-  });
+      $crossprocess.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.crossProcess().render();
+        });
+      });
 
-  $love.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.love().render();
-    });
-  });
+      $love.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.love().render();
+        });
+      });
 
-  $grungy.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.grungy().render();
-    });
-  });
+      $grungy.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.grungy().render();
+        });
+      });
 
-  $jarques.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.jarques().render();
-    });
-  });
+      $jarques.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.jarques().render();
+        });
+      });
 
-  $pinhole.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.pinhole().render();
-    });
-  });
+      $pinhole.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.pinhole().render();
+        });
+      });
 
-  $oldboot.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.oldBoot().render();
-    });
-  });
+      $oldboot.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.oldBoot().render();
+        });
+      });
 
-  $glowingsun.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.glowingSun().render();
-    });
-  });
+      $glowingsun.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.glowingSun().render();
+        });
+      });
 
-  $hazydays.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.hazyDays().render();
-    });
-  });
+      $hazydays.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.hazyDays().render();
+        });
+      });
 
-  /* Calling multiple filters inside same function */
-  $hdr.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.contrast(10);
-      this.contrast(10);
-      this.jarques();
-      this.render();
-    });
-  });
+      /* Calling multiple filters inside same function */
+      $hdr.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.contrast(10);
+          this.contrast(10);
+          this.jarques();
+          this.render();
+        });
+      });
 
-  /* Custom filters that we created */
-  $oldpaper.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.oldpaper();
-      this.render();
-    });
-  });
+      /* Custom filters that we created */
+      $oldpaper.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.oldpaper();
+          this.render();
+        });
+      });
 
-  $pleasant.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.pleasant();
-      this.render();
-    });
-  });
+      $pleasant.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.pleasant();
+          this.render();
+        });
+      });
 
-  /* You can also save it as a jpg image, extension need to be added later after saving image. */
+      /* You can also save it as a jpg image, extension need to be added later after saving image. */
 
-  $save.on('click', function(e) {
-    Caman('#canvas', img, function() {
-      this.render(function() {
-        this.save('png');
+      $save.on('click', function(e) {
+        Caman('#canvas', img, function() {
+          this.render(function() {
+            this.save('png');
+          });
+        });
       });
     });
-  });
-});
